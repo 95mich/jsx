bcec694035f20d4661cd54608b82bba703c39378
95mich <1995ichsan@gmail.com>
2023-10-20 14:35:37 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //   let message: "Panjul!";
+  //   if (Math.random() > 0.5) {
+  //     message = "Ijul!";
+  //   }
+
+  //   return <h1>{message}</h1>;
+  // }
+
+  // return (
+  //   <input
+  //     style={{ border: "2px solid blue" }}
+  //     type="number"
+  //     min={0}
+  //     max={10}
+  //   />
+  // );
+
+  return <textarea autoFocus maxLength={5} spellCheck />;
 }
 
 export default App;
